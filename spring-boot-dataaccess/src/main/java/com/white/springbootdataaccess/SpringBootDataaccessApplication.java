e9054be629e2765e0a2d449ef5935868c3b5f32f
Liuwhite <2832621940@qq.com>
2024-06-21 17:22:33 +0800

    整合数据源

---
+++ spring-boot-dataaccess/src/main/java/com/white/springbootdataaccess/SpringBootDataaccessApplication.java
@@ -0,0 +1,13 @@
+package com.white.springbootdataaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataaccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataaccessApplication.class, args);
+	}
+
+}
---
+++ spring-boot-dataaccess/src/test/java/com/white/springbootdataaccess/SpringBootDataaccessApplicationTests.java
@@ -0,0 +1,27 @@
+package com.white.springbootdataaccess;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class SpringBootDataaccessApplicationTests {
+
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Test
+	void contextLoads() throws SQLException {
+		Connection connection = dataSource.getConnection();
+		System.out.println(connection);
+	}
+
+}
